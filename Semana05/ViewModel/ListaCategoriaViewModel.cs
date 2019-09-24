b5c5d4adc38b3c4ff7eea6fac3978ff7305c7014
Jose Luis Cristobal <jose.cristobal@techie.pe>
2019-09-23 19:46:34 -0500

    Cambios Semana 6

---
+++ Semana05/ViewModel/ListaCategoriaViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Semana05.ViewModel
+{
+    public class ListaCategoriaViewModel : ViewModelBase
+    {
+        ObservableCollection<Categoria> _Categorias;
+        public ObservableCollection<Categoria> Categorias { get { return _Categorias; }
+            set
+            {
+                if (_Categorias != value)
+                {
+                    _Categorias = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand NuevoCommand { get; set; }
+        public ICommand ConsultarCommand { get; set; }
+
+        public ListaCategoriaViewModel()
+        {
+            Categorias = new Model.CategoriaModel().Categorias;
+            NuevoCommand = new RelayCommand<Window>(
+                param => Abrir()
+                
+                );
+            ConsultarCommand = new RelayCommand<Window>(
+                o => { Categorias = (new Model.CategoriaModel()).Categorias; }
+                );
+
+            void Abrir()
+            {
+                View.ManCategoria window = new View.ManCategoria();
+                window.ShowDialog();
+                ConsultarCommand.Execute(window);
+            }
+            //void Cerrar()
+            //{
+            //    View.ManCategoria window = new View.ManCategoria();
+            //    window.Close();
+            //}
+        }
+    }
+}
---
+++ Semana05/ViewModel/ManCategoriaViewModel.cs
@@ -0,0 +1,108 @@
+﻿using Entity;
+using Semana05.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Semana05.ViewModel
+{
+    public class ManCategoriaViewModel : ViewModelBase
+    {
+        #region propiedades
+        public List<Categoria> Categorias { get; set; }
+
+        int _ID;
+        public int ID
+        {
+            get { return _ID; }
+            set
+            {
+                if (_ID != value)
+                {
+                    _ID = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        string _Nombre;
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set
+            {
+                if (_Nombre != value)
+                {
+                    _Nombre = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        string _Descripcion;
+        public string Descripcion
+        {
+            get { return _Descripcion; }
+            set
+            {
+                if (_Descripcion != value)
+                {
+                    _Descripcion = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+
+        public ICommand GrabarCommand { set; get; }
+        public ICommand EliminarCommand { set; get; }
+        public ICommand CerrarCommand { set; get; }
+
+        public ICommand ListarCommand { set; get; }
+
+        public ManCategoriaViewModel()
+        {
+            GrabarCommand = new RelayCommand<Window>(
+                param =>
+                {
+                    if (ID > 0)
+                        new CategoriaModel().Actualizar(new Entity.Categoria
+                        {
+                            IdCategoria = ID,
+                            NombreCategoria = Nombre,
+                            Descripcion = Descripcion
+                        });
+                    else
+                        new CategoriaModel().Insertar(new Entity.Categoria
+                        {
+                            NombreCategoria = Nombre,
+                            Descripcion = Descripcion
+                        });
+
+                    CerrarCommand.Execute(param);
+                });
+
+            CerrarCommand = new RelayCommand<Window>(
+                param => Cerrar(param)
+                );
+
+            //ListarCommand = new RelayCommand<object>(
+            //    o => { Categorias = (new CategoriaModel()).Listar(); }
+            //    );
+        }
+
+        void Cerrar(Window window)
+        {
+            window.Close();
+        }
+
+
+    }
+}
